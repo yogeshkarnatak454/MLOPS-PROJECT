48fd2d8b9ce9b37c5d52a520b407dc7158f8afb0
yogesh karnatak <yogesh454@gmail.com>
2026-06-10 16:48:18 +0530

    project structured ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="yogesh karnatak",
+    author_email="yogeshkaranatak454@gamil.com",
+    packages=find_packages()
+)
